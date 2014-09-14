58bffd796e44d5497428786cd9198e9305af6ef5
Matt Hayward <matt@viblio.com>
2014-09-13 22:46:12 -0700

    Saving progress

--- NutritionCalc.py
+++ NutritionCalc.py
@@ -88,19 +88,20 @@ class Meal( object ):
     * food_items - A dictionary of FoodItem.id's -> amount in grams
     * nutrient_constraints - A list of constraints on nutrients for this meal
     * food_item_constraints - A list of constraints on food items for this meal
-    * nutrient_goals - A list of goals for nutrients in this meal
-    * food_item_goals - A list of goals for floor items in this meal
+    * nutrient_goals - A dictionary of nutrient-> amount goals for this meal
+    * food_item_goals - A dictionary of food_item_id -> amount goals for this meal
 
     Methods:
-    add_nutrient_constraint( nutrient, type, value )
-    add_food_item_constraint( food_item_id, type, value )
+    add_food_item( food_item_id, amount )
+    add_nutrient_constraint( nutrient, constraint_type, amount )
+    add_food_item_constraint( food_item_id, type, amount )
     type is one of 'eq' or 'limits'
-    If type is 'eq' value is a number expressed in units of the Nutrients object.
-    If type is 'limits' value is a ( lower_bound, upper_bound ) tuple,
-    where a value of None means there is no bound in that direction.
+    If type is 'eq' amount is a number expressed in units of the Nutrients object.
+    If type is 'limits' amount is a ( lower_bound, upper_bound ) tuple,
+    where an amount of None means there is no bound in that direction.
 
-    add_nutrient_goal( nutrient, value )
-    add_nutrient_goal( food_item_id, value )
+    add_nutrient_goal( nutrient, amount )
+    add_nutrient_goal( food_item_id, amount )
 
     get_food_items() - Returns an array of ( food_item, amount ) tuples.
     get_nutrients() - Returns a dictionary of nutrient->amount values
@@ -110,34 +111,104 @@ class Meal( object ):
     and we insist calories have a limit of 800 and that 110 grams of
     french fries be in the meal, there will be no solution (baring
     negative calorie food items).
-
     '''
 
     def __init__( self,
                   name,
                   food_items = [],                     # List of ( food_item_id, amount in grams )
-                  nutrient_equality_constraints = [],  # List of ( nutrient_name, value ) 
+                  nutrient_equality_constraints = [],  # List of ( nutrient_name, amount ) 
                   nutrient_limit_constraints = []      # List of ( nutrient_name, lower, upper )
-                  food_item_equality_constraints = [], # List of ( food_item_id, value ) 
+                  food_item_equality_constraints = [], # List of ( food_item_id, amount ) 
                   food_item_limit_constraints = []     # List of ( food_item_id, lower, upper )
-                  nutrient_goals = [],                 # List of ( nutrient_name, goal value )
-                  food_item_goals = [] ):              # List of ( food_item_id, goal value )
+                  nutrient_goals = [],                 # List of ( nutrient_name, goal amount )
+                  food_item_goals = [] ):              # List of ( food_item_id, goal amount )
         '''
         '''
         
         self.name = name
         
-        FIs = FoodItems()
-
+        self.FIs = FoodItems()
         self.food_items = {}
         for food_item_id, amount in food_items:
-           if FIs.get_food_item( food_item_id ): 
-               self.food_items[food_item_id] = amount
+            self.add_food_item( food_item_id, amount )
                
+        self.NUTs = Nutrients().get_nutrient_types()
         self.nutrient_constraints = []
-        for nutrient_equality_constraint in nutrient_equality_constraints:
-            
+        for nutrient, amount in nutrient_equality_constraints:
+            self.add_nutrient_constraint( nutrient, 'eq', amount )
 
+        for nutrient, lower_bound, upper_bound in nutrient_limit_constraints:
+            self.add_nutrient_constraint( nutrient, 'limits', ( lower_bound, upper_bound ) )
+
+        self.food_item_constraints = []
+        for food_item_id, amount in food_item_equality_constraints:
+            self.add_food_item_constraint( food_item_id, 'eq', amount )
+
+        for food_item_id, lower_bound, upper_bound in food_item_limit_constraints:
+            self.add_food_item_constraint( food_item_id, limits, ( lower_bound, upper_bound ) )
+
+        self.nutrient_goals = {}
+        for nutrient, amount in nutrient_goals:
+            self.add_nutrient_goal( nutrient, amount )
+
+        self.food_item_goals = {}
+        for food_item_id, amount in food_item_goals:
+            self.add_food_item_goal( food_item_id, amount )
+
+    def add_food_item( self, food_item_id, amount ):
+        if self.FIs.get_food_item( food_item_id ): 
+            self.food_items[food_item_id] = amount
+
+    def add_nutrient_constraint( self, nutrient, constraint_type, amount ):
+        if nutrient not in self.NUTs:
+            raise Exception( "No such nutrient: %s, valid nutrients are: %s" % ( nutrient, self.NUTs.keys() ) )
+
+        if constraint_type == 'eq':
+            self.nutrient_constraints.append( ( nutrient, 'eq', amount ) )
+        elif constraint_type == 'limits':
+            lower_bound = amount[0]
+            upper_bound = amount[1]
+            if lower_bound is not None:
+                self.nutrient_constraints.append( nutrient, 'gt', lower_bound )
+            if upper_bound is not None:
+                self.nutrient_constraints.append( nutrient, 'lt', upper_bound )
+
+    def add_food_item_constraint( self, food_item_id, constraint_type, amount ):
+        if self.FIs.get_food_item( food_item_id ):
+            if constraint_type == 'eq':
+                self.food_item_constraints.append( ( food_item_id, 'eq', amount ) )
+            elif constraint_type == 'limits':
+                lower_bound = amount[0]
+                upper_bound = amount[1]
+                if lower_bound is not None:
+                    self.food_item_constraints.append( food_item_id, 'gt', lower_bound )
+                if upper_bound is not None:
+                    self.food_item_constraints.append( food_item_id, 'lt', upper_bound )
+
+    def add_nutrient_goal( self, nutrient, amount ):
+        if nutrient not in self.NUTs:
+            raise Exception( "No such nutrient: %s, valid nutrients are: %s" % ( nutrient, self.NUTs.keys() ) )
+
+        self.nutrient_goals[nutrient] = amount
+
+    def add_food_item_goal( self, food_item_id, amount ):
+        if self.FIs.get_food_item( food_item_id ):
+            self.food_item_goals[food_item_id] = amount
+
+
+    def get_food_items( self ):
+        '''Return all our food items, in descending order of their amounts.'''
+        return sorted( [ ( a, b ) for a, b in self.food_items.items() ] key=lambda x: x[1], reverse=True)
+
+    def get_nutrients( self ):
+        result = {}
+        for fi, amount in self.food_items.items():
+            for nutrient, amount_per in self.FIs.get_food_item( fi ).get_nutrients().items():
+                if nutrient in result:
+                    result[nutrient] += amount * amount_per
+                else:
+                    result[nurtient] = amount * amount_per
+        return result
 
 class FoodItem( object ):
     '''Something that can be added to a meal in some quantity.
@@ -197,10 +268,28 @@ class Recipe( FoodItem ):
 # Then its keys are the nutrient names (give or take - we don't care
 # about id, uuid, created_date, ...)
 
+class Nutrients( object ):
+    '''Singleton-esque class for managing our Nutrients and their units.'''
 
+    # Class level variable that defines what our possible nutrients
+    # are, and their units.
+    nutrients = {
+        'energy' => 'kcal', # Not technically a nutrient, but convienient.
+        'fat' => 'grams',
+        'carbs' => 'grams',
+        'protein' => 'grams',
+        'vitamin C' => 'grams',
+        'calcium' => 'grams'
+    }
+
+    def __init__( self ):
+        self.nutrients = Nutrients.nutrients
+
+    def get_nutrient_types( self ):
+        return self.nutrients
 
 class FoodItems( object ):
-    '''Singleton class for managing our database of FoodItems and Recipes.
+    '''Singleton-eqsue class for managing our database of FoodItems and Recipes.
     
     Members:
     food_items - Dictionary of food_item / recipe uuid -> FoodItem
